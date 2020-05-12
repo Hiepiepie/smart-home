33712c7fca5b3358ef5106a455252cbe55e1d601
Trung Hieu Le <Le.T.Hieu@stud.h-da.de>
2020-05-12 11:20:34 +0200

    reconstruct

---
+++ src/main/java/UDPClientServer/UDPClient.java
@@ -0,0 +1,46 @@
+package UDPClientServer;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Random;
+
+public abstract class UDPClient {
+    protected int port;
+    protected InetAddress ia;
+    protected DatagramPacket dp;
+    protected DatagramSocket ds;
+    protected Random rand;
+    protected byte[] buffer;
+    protected int id;
+
+    public UDPClient() throws Exception{
+        ia = InetAddress.getLocalHost();
+        port = 1234;
+        ds = new DatagramSocket();
+        rand = new Random();
+        id = 1;
+        buffer = new byte[1024];
+        dp = new DatagramPacket(buffer, buffer.length, ia, port);
+    }
+
+    public DatagramPacket getDp(){
+        return dp;
+    }
+    public abstract String getType();
+    public abstract String getInfoUpdate();
+
+    public void sendPackage(String msg) throws Exception{
+        //msg will be in Form like : (SensorData ID);(Sensor Type);(SensorData Information)
+        // ex : 122;Humidity;50%
+        id++;
+        if (id >= 999) id = 1;
+        buffer = (msg).getBytes();
+        dp = new DatagramPacket(buffer, buffer.length, ia, port);
+        ds.send(dp);
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/main/java/mainClass.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class mainClass {
+
+    public static void main(String[] args) throws Exception {
+        String separator = File.separator;
+        separator = System.getProperty("file.separator");
+        String PATH = "target" + separator+"classes" ;
+
+        ProcessBuilder pbHygrometer = new ProcessBuilder("java", "-cp", PATH , "UDPClientServer.Hygrometer");
+        ProcessBuilder pbLight = new ProcessBuilder("java", "-cp", PATH , "UDPClientServer.Light");
+        ProcessBuilder pbThermometer = new ProcessBuilder("java", "-cp", PATH , "UDPClientServer.Thermometer");
+        ProcessBuilder pbUDPServer = new ProcessBuilder("java", "-cp", PATH , "UDPClientServer.UDPServer");
+
+        Process pHygrometer = pbHygrometer.inheritIO().start();
+        Process pLight = pbLight.inheritIO().start();
+        Process pThermometer = pbThermometer.inheritIO().start();
+        Process pUDPServer = pbUDPServer.inheritIO().start();
+
+        pUDPServer.waitFor();
+        pHygrometer.waitFor();
+        pLight.waitFor();
+        pThermometer.waitFor();
+//        String stop = "";
+//        while(!(stop.equals("x"))) {
+//
+//        }
+//
+//        pHygrometer.destroy();
+//        pLight.destroy();
+//        pThermometer.destroy();
+//        pUDPServer.destroy();
+//        System.out.println("All Devices and Server are shutted down");
+    }
+}
+
+
+
